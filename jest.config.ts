4b0e4b6182c0ca71a9a2cbd6a0e7eb51fcaef335
RaenonX <raenonx0710@gmail.com>
2021-02-10 06:22:45 -0600

    ADD - Send coverage report
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,25 @@
 import type {Config} from '@jest/types';
 
 const config: Config.InitialOptions = {
+  // Basic options
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
   },
+  // Test setup
   globalSetup: './test/setup.ts',
   globalTeardown: './test/teardown.ts',
+  // Coverage
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/**/*.ts',
+    '!**/node_modules/**',
+    '!./src/**/*.test.ts',
+    '!./src/api-def/**/*.ts',
+  ],
+  coverageDirectory: '.',
+  coverageReporters: ['clover'],
 };
 // noinspection JSUnusedGlobalSymbols
 export default config;
