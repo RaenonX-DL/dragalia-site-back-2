ef5755ee6616f08a6b26d51107935272adb18c5c
RaenonX <raenonx0710@gmail.com>
2021-05-11 06:53:04 -0500

    IMP - CORS origin to be assigned in env vars instead
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/appCors.ts
+++ src/appCors.ts
@@ -2,13 +2,20 @@ import cors, {CorsOptions} from 'cors';
 import {RequestHandler} from 'express';
 
 export const corsMiddle = (): RequestHandler => {
+  const allowedOriginsEnv = process.env.CORS_ALLOWED_ORIGINS;
+
+  if (!allowedOriginsEnv) {
+    console.error('Specify allowed CORS origins as `allowedOrigins` in env vars.');
+    process.exit(1);
+  }
+
+  const allowedOrigins = allowedOriginsEnv.split(',');
+
+  console.log('Allowed Origins: ', allowedOrigins);
+
   // Setup CORS
   const options: CorsOptions = {
-    origin: [
-      'https://dl.raenonx.cc',
-      'http://localhost:3000',
-      'http://localhost:5000',
-    ],
+    origin: allowedOrigins,
     methods: ['GET', 'POST'],
   };
   return cors(options);
