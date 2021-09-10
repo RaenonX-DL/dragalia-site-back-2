ff6a4208bc99c0b9039ca92c81af2bc1ec678d08
RaenonX <raenonx0710@gmail.com>
2021-09-10 07:05:19 -0500

    FIX - Failing tests cause by 513ef1c0
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/utils/payload/post/list.ts
+++ src/utils/payload/post/list.ts
@@ -1,11 +1,9 @@
 import {SequencedPostListPayload} from '../../../api-def/api';
 import {processPayloadBase} from '../base';
 
+
 export const processPostListPayload = <T extends SequencedPostListPayload>(payload: T): T => {
   payload = processPayloadBase(payload);
-
-  payload.start = +payload.start || 0;
-  payload.limit = +payload.limit || 0;
 
   return payload;
 };
