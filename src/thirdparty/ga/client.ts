0799f8551d1bf271a8b1f1b66c65e09bee5ce23d
RaenonX <raenonx0710@gmail.com>
2022-01-06 15:51:31 -0600

    Fixed Google Analytics credentials env vars
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/thirdparty/ga/client.ts
+++ src/thirdparty/ga/client.ts
@@ -4,12 +4,22 @@ import {BetaAnalyticsDataClient} from '@google-analytics/data';
 const propertyId = '250199955';
 export const property = `properties/${propertyId}`;
 
-if (!process.env.GA_CREDENTIAL_BASE64) {
+if (!process.env.GA_CREDENTIAL_BASE64 && !process.env.CI) {
   throw new Error(
     'Specify `GA_CREDENTIAL_BASE64` containing Google Analytics Data API credential in base64-encoded JSON.',
   );
 }
 
+/**
+ * Google Analytics Data API instance.
+ *
+ * @see {@link https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema|Documentation}
+ * @type {BetaAnalyticsDataClient} Google Analytics data client
+ */
 export const gaClient = new BetaAnalyticsDataClient({
-  credentials: JSON.parse(Buffer.from(process.env.GA_CREDENTIAL_BASE64, 'base64').toString()),
+  credentials: JSON.parse((
+    process.env.GA_CREDENTIAL_BASE64 ?
+      Buffer.from(process.env.GA_CREDENTIAL_BASE64, 'base64').toString() :
+      '{}'
+  )),
 });
