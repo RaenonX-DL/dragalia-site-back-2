a4197a86fc11d646e974568e02dad2373b98c8d4
RaenonX <raenonx0710@gmail.com>
2022-01-21 23:07:40 -0600

    Updated user settings path
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/thirdparty/mail/make/base/component.tsx
+++ src/thirdparty/mail/make/base/component.tsx
@@ -30,7 +30,7 @@ export const EmailContent: React.FunctionComponent<EmailContentProps> = ({
         <h2>{title}</h2>
         <p>{content}</p>
         <footer>
-          {I18nData.mail.manageSubscription[lang]}{makeLink(GeneralPath.USER_SUBSCRIPTION)}
+          {I18nData.mail.manageSubscription[lang]}{makeLink(GeneralPath.USER_SETTINGS)}
         </footer>
       </body>
     </html>
